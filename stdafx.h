fa983ef5c40716873fc47b11f0997049846867d2
muyun <wenlzhao@gmail.com>
2014-09-29 21:35:46 +0800

    add

---
+++ stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h :include file sfor standard system include files,
+// or project specific include files that are used frequently, but are changed infrequently
+//
+/*
+ * @author Raymond 
+ *
+ * History:
+ *   Sep-08-2014 Raymond creation
+ */
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <stdio.h>
+
+//additional headers the program requires
+
+#endif
