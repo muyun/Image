691a4c6d1ba1dc8bfc1988a42e15e59acf65a714
muyun <wenlzhao@gmail.com>
2014-10-13 14:24:13 +0800

    update

--- masked_face_detection.cpp
+++ masked_face_detection.cpp
@@ -1,58 +1,115 @@
+#include "stdafx.h"
 #include "masked_face_detection.h"
 
 using namespace cv;
 using namespace std;
 
-int main( int argc, char* argv[] )
-{
-	//if( argc != 2)
-    //{
-    // cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-    // return -1;
-    //}
-	int num = 0;
-	double k = 1.645;
-    //double result[50];
-	//normazation
-	double max_mean = 0, min_mean = 1000, norm_measured_mean_pxl = 0;
-	double value = 1;
+//int DetectMask(const std::string& nir_image){
+//	double a = 0.923100;
+//	double b = 17.307700;
+//
+//	double y = getValue(visible_image );
+//	double x = getMean(nir_image);
+//
+//
+//	if ( y >=  a * x)
+//		return 1;
+//	else
+//	    return 0;
+//}
 
-	//store the result
-	FILE* result_file;
-	//char re[256];
+void SVMTrain(Image* image_ptr, int nfiles){
+	float labels[_MAX_NUM];
+	double labelsMat[_MAX_NUM][2];
 
-	//the image dir
-	string dir = "D:\\wenlong\\workspace\\proj\\MaskedfaceDetection\\MaskedfaceDetection\\forehead\\";
-	//string dir_name = "D:\\wenlong\\workspace\\proj\\FaceLivenessDetection\\FaceLivenessDetection\\forehead\\";
-	string files_name = dir + "*.jpg";
-
-	//find the related image files
-	struct _finddata_t file_info; 
-	long handle = _findfirst(files_name.c_str(), &file_info);
-
-	if(handle == -1) { 
-		printf("Cannot read file info!\n"); 
-		return -1; 
+	for(int i = 0; i<nfiles; i++){
+		printf("Source:%s\t\t%f\t\t%s\t\t%f\n", image_ptr->nir_file_name, image_ptr->nir_norm_mean,image_ptr->visible_file_name, image_ptr->visible_norm_mean, image_ptr->value);
+		
+		//set up training data
+		labels[i] = image_ptr->value;
+		labelsMat[i][0] = image_ptr->nir_norm_mean ;
+		labelsMat[i][1] = image_ptr->visible_norm_mean ;
 	}
 
-	//printf("First image name: %s\n",file_info.name);
+	//for(int i = 0; i<nfiles; i++){
+	//	printf("Source:%s\t\t%f\t\t%f\t\t%f\n", image_ptr->nir_file_name, labelsMat[i][0],labelsMat[i][1],labels[i] = image_ptr->value);
+	//	
+	//	//set up training data
+	//	labels[i] = image_ptr->value;
+	//	labelsMat[i][0] = image_ptr->nir_norm_mean ;
+	//	labelsMat[i][1] = image_ptr->visible_norm_mean ;
+	//}
 
+	//Mat 
+	Mat labelsMat(nfiles, 1, CV_8U, labels );
+	// set up svm's parameters
+	CvSVMParams params;
+	params.svm_type = CvSVM::C_SVC;
+	params.kernel_type = CvSVM::LINEAR;
+	params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+
+	////Train the SVM
+	//CvSVM SVM;
+
+	 
+	
+}
+
+
+/*
+  replace str1 with str2
+*/
+std::string GetVisibleFile(std::string& source, std::string str1, std::string  str2){
+	int position = 0;
+	
+	while((position = source.find(str1)) != -1){
+		position = source.find(str1);
+		source.replace(position, str2.length(), str2);
+	}
+
+	//cout << "source:" << source <<endl;
+
+	return source;
+}
+
+void PrintInfo(FILE* result_file,Image* image_ptr, int nfiles){
+	int i;
+	
+	for(i = 0; i < nfiles; i++){
+		printf("[DEBUG]:%s\t\t%f\n", image_ptr->nir_file_name, image_ptr->nir_norm_mean);
+		printf("[DEBUG]:%s\t\t%f\n", image_ptr->visible_file_name, image_ptr->visible_norm_mean);
+		
+		//printf("%f\n", file_ptr[i]->norm_measured_mean);
+		fprintf(result_file, "%s\t\t%f\t\t%s\t\t%f\t\t%f\n", image_ptr->nir_file_name, image_ptr->nir_norm_mean,image_ptr->visible_file_name, image_ptr->visible_norm_mean, image_ptr->value);
+		//fprintf(result_file, "%s\t\t%f\n", image_ptr->visible_file_name, image_ptr->visible_norm_mean);
+		
+		image_ptr++;
+	}
+}
+
+void NormalizeAll(Image* image_ptr, int nfiles, double nir_max, double visible_max){
+	int i;
+
+	for(i = 0; i < nfiles; i++){
+		image_ptr->nir_norm_mean = Normalize(image_ptr->nir_norm_mean, nir_max);
+		image_ptr->visible_norm_mean = Normalize(image_ptr->visible_norm_mean, visible_max);
+		image_ptr++;
+	}
+}
+
+double  Normalize(double x, double max){
+
+	return log10(x) / log10(max);
+}
+
+double GetMean(const std::string& image){
 	//store the result
-	result_file = fopen("result.txt", "a+");
-	//fprintf(result_file, "Image_Name ");
-	fprintf(result_file, "%s\t\t%s\n", "Image_Name", "Average_gray_value");
 
-while(_findnext(handle, &file_info) == 0) {	
-	string file_name = dir + file_info.name ;
+	double k = 1.645;
 
-	//string file = file_info.name;
-    printf("The image name: %s \n", file_info.name );
-
-	struct Image image;
-
-    Mat gray_image;
+	Mat gray_image;
 	//get gray scale image
-    gray_image = imread(file_name, CV_LOAD_IMAGE_GRAYSCALE); // Read the file
+    gray_image = imread(image, CV_LOAD_IMAGE_GRAYSCALE); // Read the file
 
     if(! gray_image.data ) // Check for invalid input
     {
@@ -77,8 +134,8 @@ while(_findnext(handle, &file_info) == 0) {
 
 	//cout << "mean value: " <<mean_pxl <<endl;
 	//cout << "Standard deviation: " << stddev_pxl <<endl;
-	printf("mean value: %u\n", mean_pxl);
-	printf("Standard deviation: %u\n", stddev_pxl);
+	printf("mean value: %u; Standard deviation: %u\n", mean_pxl, stddev_pxl);
+	//printf("Standard deviation: %u\n", stddev_pxl);
 
 	int rows = gray_image.rows;
 	int cols = gray_image.cols;
@@ -94,6 +151,7 @@ while(_findnext(handle, &file_info) == 0) {
 	//double std_dev = (double)stddev_pxl;
 	//printf("std_dev: %f\n", std_dev);
 	//printf("mean: %d\n", gray_image.at<uchar>(0,0));
+	int num = 0;
 	for(int i=0; i<rows; i++){
 		for(int j=0; j<cols; j++)
 		{
@@ -103,6 +161,7 @@ while(_findnext(handle, &file_info) == 0) {
 			if( outlier_value <= k * (double)stddev_pxl){
 				//take the pixel
 			    measured_image.at<uchar>(i,j) = gray_image.at<uchar>(i,j);
+				num ++ ;
 			} else{
 				//outlier
 			     measured_image.at<uchar>(i,j) = 0;
@@ -112,42 +171,174 @@ while(_findnext(handle, &file_info) == 0) {
 
 	//imshow("forhead region", measured_image);
 
-    Scalar measured_mean,measured_stddev; 
-	meanStdDev(measured_image, measured_mean, measured_stddev);
+    //Scalar measured_mean,measured_stddev; 
+	//meanStdDev(measured_image, measured_mean, measured_stddev);
 	//Scalar measured_mean = mean(measured_image);
 	//uchar measured_mean_pxl = measured_mean.val[0];
+	double sum = 0;
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			sum = sum + measured_image.at<uchar>(i,j);
+		}
+	}
 
-	printf("measured mean : %f \n", measured_mean.val[0] );
+	//printf("measured mean : %f \n", measured_mean.val[0] );
 
-	strcpy(image.name, file_info.name);
-	image.measured_mean = (double)measured_mean.val[0];
-
-	fprintf(result_file, "%s\t\t%f\n", file_info.name, measured_mean.val[0]);
+	/*strcpy(image.name, file_info.name);
+	image.measured_mean = (double)measured_mean.val[0];*/
+	//std::cout<<image<<std::endl;
+	//fprintf(result, "%s\t\t%f\n", image.c_str(), measured_mean.val[0]);
 	//fprintf(result_file, "%f\n", measured_mean.val[0]);
 
-	if((double)measured_mean.val[0] >= max_mean){
-		max_mean = (double)measured_mean.val[0];
+	//if((double)measured_mean.val[0] >= max_mean){
+	//	max_mean = (double)measured_mean.val[0];
 
-	}
+	//}
 
-	if((double)measured_mean.val[0] <= min_mean){
-		min_mean = (double)measured_mean.val[0];
-	}
+	//if((double)measured_mean.val[0] <= min_mean){
+	//	min_mean = (double)measured_mean.val[0];
+	//}
+
 
 	//free memory
 	gray_image.release();
 	measured_image.release(); 
+
+	return sum / num;
 }
 
-    value = max_mean - min_mean;
+int main( int argc, char* argv[] )
+{
+	//if( argc != 2)
+ //   {
+ //    cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+ //    return -1;
+ //   }
+
+//	int num = 0;
+	//double k = 1.645;
+    //double result[50];
+	//normazation
+	double nir_max_mean = 0;
+	double visible_max_mean = 0;
+	//double min_mean = 256;
+//	double value = 1;
+//
+	//store the result
+	FILE* result_file;
+	//char re[256];
+//
+//	//the image dir
+    string dir = "D:\\wenlong\\workspace\\proj\\MaskedfaceDetection\\MaskedfaceDetection\\trainings\\";
+//	//string dir_name = "D:\\wenlong\\workspace\\proj\\FaceLivenessDetection\\FaceLivenessDetection\\forehead\\";
+    string files_name = dir + "*_NIR.jpg";
+//
+//	//find the related image files
+	struct _finddata_t file_info; 
+	long handle = _findfirst(files_name.c_str(), &file_info);
+
+	if(handle == -1) { 
+		printf("Cannot read file info!\n"); 
+		return -1; 
+	}
+
+	//printf("First image name: %s\n",file_info.name);
+
+		//store the result
+	result_file = fopen("result.txt", "a+");
+	//fprintf(result_file, "Image_Name ");
+	fprintf(result_file, "%s\t\t%s\t\t%s\t\t%s\t\t%s\n", "NIR_Image_Name", "nir_norm_mean", "visible_Image_name", "visible_norm_mean", "value");
+
+//	string file_name = dir + file_info.name ;
+
+	//string file = file_info.name;
+    //printf("The image name: %s \n", file_info.name );
+
+	int nfiles = 0; //files num
+	Image image[_MAX_NUM];
+
+while(_findnext(handle, &file_info) == 0) {
+	string nir_file_name_ = dir + file_info.name;
+	printf("The Nir image: %s \n", file_info.name);
+//
+//	struct Image image;
+	//string dir = "D:\\wenlong\\workspace\\proj\\MaskedfaceDetection\\MaskedfaceDetection\\forehead\\";
+	//string image1 = dir + "processed_head_2014-9-17-11-30-19_NIR.jpg";
+	//string image2 = dir + "processed_head_2014-9-17-11-30-19_visible.jpg";
+	//int result = DetectMask(image1, image2);
+
+	double nir_mean = GetMean(nir_file_name_);
+	//fprintf(result_file, "%s\t\t%f\n", file_info.name, mean);
+	//printf("Info:%s\t\t%f\n", file_info.name, mean);
+	if(nir_mean > nir_max_mean)
+		nir_max_mean = nir_mean;
+
+	//visible 
+	string nir_name = file_info.name;
+	string visible_file_name_ = GetVisibleFile(nir_name, "NIR", "visible" ) + ".jpg";
+	cout<< "The visible file:" << visible_file_name_ <<endl;
+	double visible_mean = GetMean(dir + visible_file_name_);
+	//visible_max_mean
+
+	if(visible_mean > visible_max_mean)
+		visible_max_mean = visible_mean;
+
+	//if(nir_mean < min_mean)
+	//	min_mean = nir_mean;
+
+	//image_ptr = (Image*)malloc(sizeof(Image));
+	//memset copies '\0' to the first sizeof characters of the string pointed to by the argument
+	int num = nfiles++;
+	//char* file_name_ptr = image[num].file_name;
+	memset(image[num].nir_file_name, '\0', sizeof(char) * _MAX_FNAME);
+	strcpy(image[num].nir_file_name, file_info.name);
+	//printf("image:%s\n", image[nfiles++].name);
+	memset(image[num].visible_file_name, '\0', sizeof(char) * _MAX_FNAME);
+	strcpy(image[num].visible_file_name, visible_file_name_.c_str());
+
+	image[num].nir_norm_mean = nir_mean;
+	image[num].visible_norm_mean = visible_mean;
+	//printf("image:%s\t\t%f\n", image[num].file_name, image[num].norm_measured_mean);
+	//fprintf(result_file, "%s\t\t%f\n", image[num].file_name, image[num].norm_measured_mean);
+	/*file_ptr[nfiles++] = &image;
+
+	printf("file_ptr:%s\t\t%f\n", file_ptr[nfiles]->name, file_ptr[nfiles]->norm_measured_mean);*/
+	//cout<< "result: "<< result <<endl;
+
+	// label the data
+	if((nir_name.find("processed") != string::npos) || (visible_file_name_.find("processed") != string::npos) ){
+		//find -- real
+		//printf("real\n:", nir_name);
+		//printf("real\n:", visible_file_name_);
+		image[num].value = 0;
+	} else {
+		//masked
+		//printf("Masked\n:", nir_name);
+		//printf("Masked\n:", visible_file_name_);
+	    image[num].value = 1;
+	}
+	
+
+}
+    
+    //printf("max_mean: %f\n", max_mean);
+	
+    // normalize
+    NormalizeAll(image, nfiles, nir_max_mean, visible_max_mean);
+	
+	PrintInfo( result_file, image, nfiles);
+    //value = max_mean - min_mean;
 
     fprintf(result_file, "end.\n");
 
     _findclose(handle); // close
 
-    waitKey(0); // Wait for a keystroke in the window
+    //waitKey(0); // Wait for a keystroke in the window
 
 	fclose(result_file);
+
+	//training
+	SVMTrain(image, nfiles);
 
     return 0;
 }
--- masked_face_detection.h
+++ masked_face_detection.h
@@ -1,153 +1,50 @@
-#include "masked_face_detection.h"
+#ifndef MASKED_FACE_DETECTION_H
+#define MASKED_FACE_DETECTION_H
 
-using namespace cv;
-using namespace std;
+//#include <iostream>
+//#include <string>
 
-int main( int argc, char* argv[] )
-{
-	//if( argc != 2)
-    //{
-    // cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-    // return -1;
-    //}
-	int num = 0;
-	double k = 1.645;
-    //double result[50];
-	//normazation
-	double max_mean = 0, min_mean = 1000, norm_measured_mean_pxl = 0;
-	double value = 1;
+//opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/ml/ml.hpp>
 
-	//store the result
-	FILE* result_file;
-	//char re[256];
+#include <cv.h>
+#include <highgui.h>
 
-	//the image dir
-	string dir = "D:\\wenlong\\workspace\\proj\\MaskedfaceDetection\\MaskedfaceDetection\\forehead\\";
-	//string dir_name = "D:\\wenlong\\workspace\\proj\\FaceLivenessDetection\\FaceLivenessDetection\\forehead\\";
-	string files_name = dir + "*.jpg";
+//c++
+#include <string>
 
-	//find the related image files
-	struct _finddata_t file_info; 
-	long handle = _findfirst(files_name.c_str(), &file_info);
+//svm
+#include "svm.h"
 
-	if(handle == -1) { 
-		printf("Cannot read file info!\n"); 
-		return -1; 
-	}
+#define _MAX_NUM  150  // max #num to be sorted
+//char *file_ptr[_MAX_NUM];   //pointers to files; pointer arrays
 
-	//printf("First image name: %s\n",file_info.name);
+typedef struct {
+	char nir_file_name[_MAX_FNAME];  //, filename lengh, _MAX_FNAME is defined in stdlib.h
+	char visible_file_name[_MAX_FNAME];
+	double nir_norm_mean;
+	double visible_norm_mean;
+	float value;
+} Image;
 
-	//store the result
-	result_file = fopen("result.txt", "a+");
-	//fprintf(result_file, "Image_Name ");
-	fprintf(result_file, "%s\t\t%s\n", "Image_Name", "Average_gray_value");
+//file_ptr[i] is a Image pointer,
+// and *file_ptr[i] is the Image it points to, the first character of the i-th saved the file name
+//Image *file_ptr[_MAX_NUM]; //pointers to files (Image); pointer arrays
 
-while(_findnext(handle, &file_info) == 0) {	
-	string file_name = dir + file_info.name ;
+double GetMean(const std::string& image);
 
-	//string file = file_info.name;
-    printf("The image name: %s \n", file_info.name );
+bool DetectMask(const std::string& image1, const std::string& image2);
 
-	struct Image image;
+//void ReadFiles(const std::string& files);
 
-    Mat gray_image;
-	//get gray scale image
-    gray_image = imread(file_name, CV_LOAD_IMAGE_GRAYSCALE); // Read the file
+void NormalizeAll(Image* image_ptr, int nfiles, double max);
+double  Normalize(double x, double max);
+void PrintInfo(Image* image_ptr, int nfiles);
 
-    if(! gray_image.data ) // Check for invalid input
-    {
-        printf("Could not open or find the image \n");
-        return -1;
-    }
+//#define MAX_MEAN 0
+//#define MIN_MEAN 1000
 
-    //namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-   // imshow( "source image", image ); // Show our image inside it.
-	//convert the image from BGR to Grayscale format
-	//Mat gray_image;
-	//cvtColor(image, gray_image, CV_BGR2GRAY);
-	//imshow("gray image", gray_image);
-	
-	//get mean and standard deviation
-	//Scalar is a four-double-type-element array
-	Scalar mean, stddev; 
-	meanStdDev(gray_image, mean, stddev);
-
-	uchar mean_pxl = mean.val[0];
-	uchar stddev_pxl = stddev.val[0];
-
-	//cout << "mean value: " <<mean_pxl <<endl;
-	//cout << "Standard deviation: " << stddev_pxl <<endl;
-	printf("mean value: %u\n", mean_pxl);
-	printf("Standard deviation: %u\n", stddev_pxl);
-
-	int rows = gray_image.rows;
-	int cols = gray_image.cols;
-	//measured_image store the image which removes the outliers (about 10% of the radiance are outliers)
-	//printf("gray_image.type: %s\n", gray_image.type());
-	Mat measured_image(rows, cols, gray_image.type());
-	measured_image.setTo(0);
-	//
-	/*Mat convert_image;
-	gray_image.convertTo(convert_image, CV_32FC1);*/
-	//
-
-	//double std_dev = (double)stddev_pxl;
-	//printf("std_dev: %f\n", std_dev);
-	//printf("mean: %d\n", gray_image.at<uchar>(0,0));
-	for(int i=0; i<rows; i++){
-		for(int j=0; j<cols; j++)
-		{
-			//Mat::at returns a reference to the specified array element
-			double outlier_value = fabs(gray_image.at<uchar>(i,j) - (double)mean_pxl);
-//			printf("ri_value: %f", ri_value);
-			if( outlier_value <= k * (double)stddev_pxl){
-				//take the pixel
-			    measured_image.at<uchar>(i,j) = gray_image.at<uchar>(i,j);
-			} else{
-				//outlier
-			     measured_image.at<uchar>(i,j) = 0;
-			}
-		}
-	}
-
-	//imshow("forhead region", measured_image);
-
-    Scalar measured_mean,measured_stddev; 
-	meanStdDev(measured_image, measured_mean, measured_stddev);
-	//Scalar measured_mean = mean(measured_image);
-	//uchar measured_mean_pxl = measured_mean.val[0];
-
-	printf("measured mean : %f \n", measured_mean.val[0] );
-
-	strcpy(image.name, file_info.name);
-	image.measured_mean = (double)measured_mean.val[0];
-
-	fprintf(result_file, "%s\t\t%f\n", file_info.name, measured_mean.val[0]);
-	//fprintf(result_file, "%f\n", measured_mean.val[0]);
-
-	if((double)measured_mean.val[0] >= max_mean){
-		max_mean = (double)measured_mean.val[0];
-
-	}
-
-	if((double)measured_mean.val[0] <= min_mean){
-		min_mean = (double)measured_mean.val[0];
-	}
-
-	//free memory
-	gray_image.release();
-	measured_image.release(); 
-}
-
-    value = max_mean - min_mean;
-
-    fprintf(result_file, "end.\n");
-
-    _findclose(handle); // close
-
-    waitKey(0); // Wait for a keystroke in the window
-
-	fclose(result_file);
-
-    return 0;
-}
+#endif
